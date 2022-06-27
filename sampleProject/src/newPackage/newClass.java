454c5b8ff8f9e31e2a0146bef630f5daccbc8d2f
kiran <kiran.ghorpade@hcl.com>
2022-06-27 11:43:27 +0530

    added result output

--- sampleProject/src/newPackage/newClass.java
+++ sampleProject/src/newPackage/newClass.java
@@ -5,6 +5,10 @@ public class newClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hii");
+		byte a =10;
+		byte b=10;
+		int c=a+b;
+		System.out.println("result is "+c);
 	}
 
 }
